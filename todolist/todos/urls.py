522770ffcdafb029edb462a2d5d657ac1702a8a8
sudhanshu-jha <jha.sudhanshu1991@gmail.com>
2018-05-14 18:09:22 +0530

    updated

--- todolist/todos/urls.py
+++ todolist/todos/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^add',views.add, name='add'),
     url(r'^details/(?P<id>\w{0,50})/$', views.details),
+    # url(r'^details/(?P<id>\)/$', views.details),
     url(r'^edit/(?P<pk>\d+)/detail/$',views.edit),
     url(r'^delete/(?P<pk>\d+)/detail/$',views.delete),
     
